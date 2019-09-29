c536e080577c388be79cca8139b9321b6fbd0d62
dmitry <chunindi@global.bcs.ru>
2019-09-29 21:26:31 +0300

    remove stop handler, use router and bind to it pipeline

--- src/main/java/server/NettyMain.java
+++ src/main/java/server/NettyMain.java
@@ -1,5 +1,6 @@
 package server;
 
+import ru.otus.framework.Router;
 import ru.otus.framework.el.BossEventLoop;
 import ru.otus.framework.el.EventLoop;
 import ru.otus.framework.pipeline.ChannelPipeline;
@@ -7,6 +8,8 @@ import server.handler.InverseLetterCaseHandler;
 import server.handler.PackagePayloadWithWorkerNameHandler;
 import server.handler.ReplaceAWithHeyHandler;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,11 +22,14 @@ public class NettyMain {
 //        ExecutorService workerExecutor = Executors.newFixedThreadPool(2 * Runtime.getRuntime().availableProcessors());
 
         //todo add ServerBootstrap class for client simple configuration
-        final ChannelPipeline pipeline = new ChannelPipeline();
-        pipeline.addLast(new InverseLetterCaseHandler());
-        pipeline.addLast(new ReplaceAWithHeyHandler());
-        pipeline.addLast(new PackagePayloadWithWorkerNameHandler());
-        final EventLoop bossEventLoop = new BossEventLoop(PORT, 2);
-        bossExecutor.submit(() -> bossEventLoop.go(pipeline));
+        final ChannelPipeline payloadPipeline = new ChannelPipeline();
+        payloadPipeline.addLast(new InverseLetterCaseHandler());
+        payloadPipeline.addLast(new ReplaceAWithHeyHandler());
+        payloadPipeline.addLast(new PackagePayloadWithWorkerNameHandler());
+        Router payloadRouter = Router.route(payloadPipeline).path("/some");
+
+        List<Router> routers = Collections.singletonList(payloadRouter);
+        final EventLoop bossEventLoop = new BossEventLoop(PORT, 2, routers);
+        bossExecutor.submit(bossEventLoop::go);
     }
 }
